655f67cc604ee5b243e95fd3fc2da654ec109209
Esther Weeks <esta.weeks@gmail.com>
2013-12-05 17:36:12 -0700

    Adding jscript to change calculations with filters

--- js/slider.js
+++ js/slider.js
@@ -9,10 +9,28 @@
 		$('#checkFilter').click(function() {
 			if($('#checkFilter').is(':checked')){
    				$('#selectFilter').show();
+   				//$('input:radio[name="option1"][value="oneThird"]').prop('checked', true);
+   				var filter=Math.round((Math.LOG10E*Math.log($( "#filter").val()) * 3.321928)*100)/100;
+				stop();
+				$("#filter").change(function(){
+					stop();
+				});
    			}else{
    				$('#selectFilter').hide();
    			}
-   		})
+   		});
+   		
+   		$("#filter").change(function(){
+			console.log( "change");
+			stop();
+		})
+   		//$("#selectFilter").change(function(){
+   			//var filter=Math.round((Math.LOG10E*Math.log($( "#filter").val()) * 3.321928)*100)/100;
+   		//	var third = [".9","1","1.1", "1.2","1.4","1.6","1.8","2","2.2","2.5","2.8","3.2", "3.5", "4", "4.5"];
+		//	for(var i=0; i<=12; i=i+1){
+		//		$('#ap td').eq(i).html(third[i+filter]);
+		//	}
+   		//});
 		
 		$(function() {
 			var light;
@@ -93,10 +111,10 @@
 			var start = (iso2/(Math.pow(2, c)) );
 			start = Math.round(start * 100) / 100;
 			$('#shutter li:eq(9)').html(start);
-			var filter=Math.round((Math.LOG10E*Math.log($( "#filter").val()) * 3.321928)*100)/100;
+			//var filter=Math.round((Math.LOG10E*Math.log($( "#filter").val()) * 3.321928)*100)/100;
 
 			
-			console.log("calculated, e is"+e+"iso is "+iso2 );
+			//console.log("calculated, e is"+e+"iso is "+iso2 );
 			
 			var myArray =[];
 			myArray[8] =start;
@@ -105,7 +123,7 @@
 				j=Math.round(j * 100) / 100;
 				myArray[i]=j;
 			}
-			for(var i=9; i<=11; i=i+1){
+			for(var i=9; i<=14; i=i+1){
 				var j=(myArray[i-1])/2;
 				j=Math.round(j * 100) / 100
 				myArray[i]=j;
@@ -118,7 +136,7 @@
 			function printElementAndIndex( elem, index ) {
 				console.log( "Index " + index + ": " + elem );
 			}
-			myArray.forEach( printElementAndIndex );
+			//myArray.forEach( printElementAndIndex );
 			
 		}
 
@@ -133,6 +151,7 @@
 		$( "select" ).change(displayVals);
 		displayVals();
 		changeScale();
+
 		//changeScale();
 		//function filter(){
 			//var filt = $( "#filter").val();
@@ -141,24 +160,71 @@
 		function changeScale(){
 			
 			$('#stop').click(function(){
-				if($('#oneHalf').is(':checked')) {
-					var half = [".8","1","1.2", "1.4","1.7","2","2.4","2.8","3.3","4","4.8","5.6"];
+				stop();
+			});
+		}
+		
+		function stop(){
+			if($('#oneHalf').is(':checked')) {
+					var half = [".8","1","1.2", "1.4","1.7","2","2.4","2.8","3.3","4","4.8","5.6", "b", "c", "d", "d"];
 					for(var i=0; i<=12; i=i+1){
-						$('#ap td').eq(i).html(half[i]);
+						//$('#ap td').eq(i).html(half[i]);
+						if($('#checkFilter').is(':checked')){
+							var filter=Math.round((Math.LOG10E*Math.log($( "#filter").val()) * 3.321928)*100)/100;
+							$('#ap td').eq(i).html(half[i+filter]);
+						}else{
+							$('#ap td').eq(i).html(half[i]);
+						}
 					}
 				}else if($('#oneThird').is(':checked')){
-					var full = [".9","1","1.1", "1.2","1.4","1.6","1.8","2","2.2","2.5","2.8","3.2"];
+					var third = [".9","1","1.1", "1.2","1.4","1.6","1.8","2","2.2","2.5","2.8","3.2"];
 					for(var i=0; i<=12; i=i+1){
-						$('#ap td').eq(i).html(full[i]);
+						if($('#checkFilter').is(':checked')){
+							var filter=Math.round((Math.LOG10E*Math.log($( "#filter").val()) * 3.321928)*100)/100;
+							$('#ap td').eq(i).html(third[i+filter]);
+						}else{						
+							$('#ap td').eq(i).html(third[i]);
+						}
 					}					
 				}else{
 					var full = ["1","1.4","2", "2.8","4","5.6","8","11","16","22","32","45"];
 					for(var i=0; i<=12; i=i+1){
-						$('#ap td').eq(i).html(full[i]);
+						if($('#checkFilter').is(':checked')){
+							var filter=Math.round((Math.LOG10E*Math.log($( "#filter").val()) * 3.321928)*100)/100;
+							$('#ap td').eq(i).html(full[i+filter]);
+						}else{
+							$('#ap td').eq(i).html(full[i]);
+						}
 					}					
 				}
-			});
 		}
+		
+				
+		$('#checkFilter').click(function() {
+			if($('#checkFilter').is(':checked')){
+   				$('#selectFilter').show();
+   				//$('input:radio[name="option1"][value="oneThird"]').prop('checked', true);
+   				var filter=Math.round((Math.LOG10E*Math.log($( "#filter").val()) * 3.321928)*100)/100;
+				stop();
+				$("#filter").change(function(){
+					stop();
+				});
+   			}else{
+   				$('#selectFilter').hide();
+   			}
+   		});
+   		
+   		$("#filter").change(function(){
+			console.log( "change");
+			stop();
+		})
+   		//$("#selectFilter").change(function(){
+   			//var filter=Math.round((Math.LOG10E*Math.log($( "#filter").val()) * 3.321928)*100)/100;
+   		//	var third = [".9","1","1.1", "1.2","1.4","1.6","1.8","2","2.2","2.5","2.8","3.2", "3.5", "4", "4.5"];
+		//	for(var i=0; i<=12; i=i+1){
+		//		$('#ap td').eq(i).html(third[i+filter]);
+		//	}
+   		//});
 		
 		});
 		
