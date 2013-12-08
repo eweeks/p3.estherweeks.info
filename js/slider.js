d023dea2f5ada73e79b79de663751570528dbc6e
Esther Weeks <esta.weeks@gmail.com>
2013-12-07 21:18:05 -0700

    adjusted calculations to better deal with higher and lower lighting, added few error checks

--- js/slider.js
+++ js/slider.js
@@ -14,12 +14,15 @@ On load - clears inputs, resets
 		});
 		//Variables used through out 
 		var lastStop;
+		var start;
 		var fullStops= ["64000", "32000","16000","8000", "4000","2000","1000","500","250","125",
-		"60","30","15", "8", "4", "2", '1', '2\"', '4\"', '8\"', '15\"', '30\"' ];
-		var halfStops=["3000", "2000", "1500", "1000", "750", "500", "350", "250", "180",
-		"125", "90", "60", "45", "30", "20", "15", "11", "8", "6", "4", "3", "2"];
+		"60","30","15", "8", "4", "2", '1\"', '2\"', '4\"', '8\"', '15\"', '30\"' ];
+		var halfStops=["12000","8000","6000","4000", "3000", "2000", "1500", "1000", "750", "500", "350", "250", "180",
+		"125", "90", "60", "45", "30", "20", "15", "10", "8", "6", "4", "3", "2", "1.5", '1\"', '1.5\"',
+		'2\"', '3\"', '4\"', '6\"', '8\"', '10\"'];
 		var thirdStops=["2000", "1600", "1250", "1000", "800", "640", "500",
-		"400", "320", "250", "200", "160", "125", "100", "80", "60", "50", "40", "30", "25"];
+		"400", "320", "250", "200", "160", "125", "100", "80", "60", "50", "40", "30", "25", "20", "15", "13",
+		"10", "8", "6", "5", "4", "3", "2.5", "2", "1.6", "1.3", '1\"', '1.3\"', '1.6\"', '2\"'];
 
 	
 /*-------------------------------------------------------------------------------------------------
@@ -28,7 +31,7 @@ Slider - using jQuery UI
 		$( "#slider" ).slider({
 			//variables from jQuery UI for slider
 			value:15,
-			min: 8,
+			min: 3,
 			max: 16,
 			step: 1,
 			//This insures that when the slider is changed, runs calculate function to update shutter speed
@@ -39,7 +42,7 @@ Slider - using jQuery UI
 			
 			slide: function( event, ui ) {
 				$( "#amount" ).val( ui.value );
-				/*if(ui.value==3){
+				if(ui.value==3){
 					$('#description').html("Streetlights, Fireworks");	
 				}else if(ui.value==4){
 					$('#description').html("Floodlit buildings, Bright Streetlights");
@@ -49,8 +52,7 @@ Slider - using jQuery UI
 					$('#description').html("Bright night interior, shady day interior");
 				}else if(ui.value==7){
 					$('#description').html("Indoor sports, stage shows");
-				}else*/ 
-				if(ui.value==8){
+				}else if(ui.value==8){
 					$('#description').html("Floodlit stadium, bright day interior");
 				}else if(ui.value==9){
 					$('#description').html("Neon lights, spot-lit subjects");
@@ -102,22 +104,24 @@ Calculate (shutter speed)
 			//formula to calculate the shutter speed
 			var c = (15-e-0.32195);
 			var newStart=lastStop;
-			var start = (iso3/(Math.pow(2, c)) );
+			start = (iso3/(Math.pow(2, c)) );
 			var point;
 			
 			console.log("starting point is "+start);
 			if(start<1){
 				start=Math.round(1/start);
+				check();
 				start=start+'\"';
 				console.log("starting point lower is "+start);
 			}else{
 				//rounds off calculation
 				start = Math.round(start);
+				check();
 			}
 			console.log("starting point is "+start);
-
 			
 			
+			/*
 			//takes starting point calculation, and converts it to the scale used on most cameras
 			if(start>=15 && start<30){
 				start=15;
@@ -131,7 +135,7 @@ Calculate (shutter speed)
 			}else if(start>=125 && start<250){
 				start=125;
 				console.log("starting point is "+start);
-			}
+			}*/
 			
 			//takes starting point from calculation, and finds where it is in correct array
 			if($('#oneHalf').is(':checked')){
@@ -156,37 +160,73 @@ Calculate (shutter speed)
 			//this sets the values above the starting point, using array with set values
 			var up=point+1;	
 			for(var i=10; i<=13; i=i+1){
-				if((up)>21){
-					$('#ss td').eq(i).html("-");
-				}else{
-					if($('#oneHalf').is(':checked')){
+				if($('#oneHalf').is(':checked')){
+					if((up)>=halfStops.length){
+						$('#ss td').eq(i).html("-");
+					}else{
 						$('#ss td').eq(i).html(halfStops[up]);
-					}else if($('#oneThird').is(':checked')){
+					}
+				}else if($('#oneThird').is(':checked')){
+					if((up)>=thirdStops.length){
+						$('#ss td').eq(i).html("-");
+					}else{
 						$('#ss td').eq(i).html(thirdStops[up]);
+					}
+				}else{
+					if((up)>=fullStops.length){
+						$('#ss td').eq(i).html("-");
+						console.log("Full Stop length is reached  "+fullStops.length);
 					}else{
 						$('#ss td').eq(i).html(fullStops[up]);
 					}
 				}
+				
 				up=up+1;
 			}
 			
 			//this sets the values below the starting point, using the array with set values
 			var down=point-1;
-			for(var i=8; i>=0; i=i-1){
-				if((down)<0){
+			//if down is already <0, it's off the chart, fill in with blanks
+			if(down<0){
+				for(var i=0; i<=13; i=i+1){
 					$('#ss td').eq(i).html("-");
-				}else{	
-					if($('#oneHalf').is(':checked')){
-						$('#ss td').eq(i).html(halfStops[down]);
-					}else if($('#oneThird').is(':checked')){
-						$('#ss td').eq(i).html(thirdStops[down]);
-					}else{
-						$('#ss td').eq(i).html(fullStops[down]);
-					}
 				}
-				down=down-1;
+			}else{
+				for(var i=8; i>=0; i=i-1){
+					if((down)<0){
+						$('#ss td').eq(i).html("-");
+					}else{	
+						if($('#oneHalf').is(':checked')){
+							$('#ss td').eq(i).html(halfStops[down]);
+						}else if($('#oneThird').is(':checked')){
+							$('#ss td').eq(i).html(thirdStops[down]);
+						}else{
+							$('#ss td').eq(i).html(fullStops[down]);
+						}
+					}
+					down=down-1;
+				}
 			}
 		}//end calculate function
+
+	//Because cameras uses scales with rounded values that don't quite fit calculations, this
+	//function checks the numbers against values in the usually camera scale
+	function check(){
+			if(start>=15 && start<30){
+				start=15;
+				console.log("starting point is "+start);
+			}else if(start>=30 && start<60){
+				start=30;
+				console.log("starting point is "+start);
+			}else if(start>=60 && start<125){
+				start=60;
+				console.log("starting point is "+start);
+			}else if(start>=125 && start<250){
+				start=125;
+				console.log("starting point is "+start);
+			}	
+	
+	}
 
 /*-------------------------------------------------------------------------------------------------
 ISO
